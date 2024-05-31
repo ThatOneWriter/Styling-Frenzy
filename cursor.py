94a375a0d509ca26dae0bd3211cd69deefdd854d
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2024-05-31 08:44:35 -0400

    5/31/24
    I was able to fix the cursor finally, but there’s still a slight problem with it, one that might resurface later on, but I’ll figure it out when the time comes. 
    
    I found a new background for the game, and over the weekend, I will create characters and add dialogue.

--- cursor.py
+++ cursor.py
@@ -6,9 +6,9 @@ class Cursor:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.image = pygame.transform.scale(pygame.image.load("cursor.png"), (30, 30))
+        self.image = pygame.transform.scale(pygame.image.load("cursor.png"), (35, 35))
         self.image_size = self.image.get_size()
-        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+
 
 
 
--- main.py
+++ main.py
@@ -15,11 +15,11 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Styling Frenzy")
 
 timer = pygame.time.Clock()
-messages = ['Ready to play?',
+messages = ['~Click the start button to begin~','Ready to play?',
             'Are you sure?',
-            'Sure sure?',
+            'No turning back you know.',
             '.', '..', '...',
-            'Alright alright, quit being so impatient..']
+            'Alright alright, quit being so impatient..', 'Jeez, kids these days...']
 t = font.render('', True, 'white')
 touch_message = 0
 message = messages[touch_message]
@@ -34,15 +34,16 @@ b = 59
 
 
 click_button = Button("click", 430, 470)
-c = Cursor(23, 30)
+c = Cursor(400, 89)
 bg = Background(210, 120)
 
 
 run = True
 
 while run:
+
     movement = pygame.mouse.get_pos()
-    print(movement)
+
     screen.fill((r, g, b))
     timer.tick(60)
     if count < sp * len(message):
@@ -51,21 +52,20 @@ while run:
         done = True
 
     for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
+        if event.type == pygame.QUIT:  # If user  clicked close
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if c.rect.colliderect(click_button.rect):
-                if done and touch_message < len(messages):
-                    touch_message += 1
-                    done = False
-                    message = messages[touch_message]
-                    count = 0
+            if done and touch_message < len(messages):
+                touch_message += 1
+                done = False
+                message = messages[touch_message]
+                count = 0
     snip = font.render(message[0:count//sp], True, "white")
 
     screen.blit(snip, (400, 89))
     screen.blit(click_button.image, click_button.rect)
     screen.blit(bg.image, bg.rect)
-    screen.blit(c.image, c.rect)
+    screen.blit(c.image, movement)
     pygame.display.flip()
     pygame.display.update()
 pygame.quit()
